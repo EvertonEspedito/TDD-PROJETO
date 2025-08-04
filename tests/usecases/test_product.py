1de9c282bfeb7d58896403f6ea2d1d4e4823f577
Everton Espedito <evertondev505@gmail.com>
2025-08-04 10:43:09 -0300

    update

--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -1,7 +1,14 @@
+from store.schemas.product import ProductOut
 from store.usecases.product import product_usecase
 
 
 async def test_usecases_should_return_success(product_in):
     result = await product_usecase.create(body=product_in)
-    #assert isinstance(result, ProductOut)
-    assert result is None
+    assert isinstance(result, ProductOut)
+    assert result.name == "Iphone 14 Pro Max"
+    
+
+async def test_usecases_get_should_return_success(product_id):
+    result = await product_usecase.get(id=product_id)
+    assert isinstance(result, ProductOut)
+    assert result.name == "Iphone 14 Pro Max"
